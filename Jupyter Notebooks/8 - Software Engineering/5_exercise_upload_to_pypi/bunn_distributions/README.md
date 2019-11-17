81433e65af54ed619bc3d3cc3e31d38a4d7c21a1
Christophe Bunn <christophe.bunn@gmail.com>
2019-11-17 16:32:33 +0100

    Initial commit of lesson 8 - Software Engineering Jupyter Notebooks

---
+++ Jupyter Notebooks/8 - Software Engineering/5_exercise_upload_to_pypi/bunn_distributions/README.md
@@ -0,0 +1,1 @@
+# Exercise: Upload to PyPi #
---
+++ Jupyter Notebooks/8 - Software Engineering/5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bunn_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bunn_distributions'],
+      author = 'Christophe Bunn',
+      author_email = 'christophe.bunn@gmail.com',
+      zip_safe=False)
