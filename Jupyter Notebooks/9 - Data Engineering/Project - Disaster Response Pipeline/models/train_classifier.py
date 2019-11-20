395211934fed3bd01d5c8a2c23275aab1429c43e
Christophe Bunn <christophe.bunn@gmail.com>
2019-11-20 22:25:40 +0100

    Moved the StartingVerbExtractor class from train_classifier.py to its own starting_verb_extractor.py.

--- Jupyter Notebooks/9 - Data Engineering/Project - Disaster Response Pipeline/models/train_classifier.py
+++ Jupyter Notebooks/9 - Data Engineering/Project - Disaster Response Pipeline/models/train_classifier.py
@@ -18,9 +18,11 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import GridSearchCV
-from sklearn.base import BaseEstimator, TransformerMixin
+#from sklearn.base import BaseEstimator, TransformerMixin
 
 import pickle
+
+from starting_verb_extractor import StartingVerbExtractor
 
 def load_data(database_filepath):
     '''
@@ -63,39 +65,6 @@ def tokenize(text):
     tokens = [lemmatizer.lemmatize(word).strip() for word in tokens if word not in stop_words]
     
     return tokens
-
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-    '''
-    Custom Transformer for detecting phrases that start with a verb or retweets.
-    It will be used to add features other than TF-IDF.
-    '''
-
-    def starting_verb(self, text):
-
-        # tokenize into sentences
-        sentence_list = nltk.sent_tokenize(text)
-
-        for sentence in sentence_list:
-
-            # tokenize into words and tag Part of Speech
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-
-            if (len(pos_tags) != 0):
-                # get the 1st word and PoS tag
-                first_word, first_tag = pos_tags[0]
-
-                # return True if the 1st word is a verb or indicates a retweet
-                if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                    return True
-
-        return False
-    
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
 
 def build_model():
     '''
